68b4d7b5ce329888d50a55f394df6230abfc9041
d36choi <yuio1126@gmail.com>
2020-03-21 21:16:42 +0900

    Initial commit

---
+++ src/main/java/com/project/refrishserver/RefrishServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.refrishserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefrishServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefrishServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/refrishserver/RefrishServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.refrishserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefrishServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
